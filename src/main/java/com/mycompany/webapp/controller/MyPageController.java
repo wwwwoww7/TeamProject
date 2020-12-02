2a27fea394729093c1e5185b7f2735b2fbc310c5
COM <COM@DESKTOP-TH36TT5>
2020-12-02 09:27:35 +0900

    혜빈이 마이페이지와 합침 - 지연

--- src/main/java/com/mycompany/webapp/controller/MyPageController.java
+++ src/main/java/com/mycompany/webapp/controller/MyPageController.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -144,41 +145,124 @@ public class MyPageController {
 		
 		ClassNoticeDto notice = classNoticeService.getNoticeDetail(class_notice_no);
 		model.addAttribute("notice",notice);
+		
+		//공지사항에 보여질 파일
+		List<ClassNoticeDto> newUpload = classNoticeService.getFiles(notice.getClass_hw_no());
+		model.addAttribute("newUpload",newUpload);
 	
 		return "mypage/noticedetail";
 	}
 	
+	//강의 공지사항의 파일 다운로드 받기
+	@GetMapping("/download")
+	public void download(String fileName, HttpServletResponse respone,HttpServletRequest request) throws IOException {
+		logger.info(fileName);
+		//파일의 데이터를 읽기 위한 입력 스트림 얻기
+		String saveFilePath = "D:/MyWokspace/files/"+fileName;
+		InputStream is = new FileInputStream(saveFilePath);
+		
+		//[응답 HTTP 헤더 구성]
+		//1. content type 헤더 구성(파일의 종류) -  브라우저가 어떤  프로그램이 실행될지 알기 위해서 
+		ServletContext application = request.getServletContext(); 
+		String fileType = application.getMimeType(fileName);
+		respone.setContentType(fileType);
+		
+		//2. content Disposition 헤더 구성 (다운로드할 파일의 이름 지정)
+		String OriginalFileName = fileName.split("_")[1]; // 숫자[0] _ 실제이름[1]
+		//헤더에서는 한글을 그대로 전달할 수 없음으로 파일이름이 한글이면 아스키코드(ISO-8859-1)로 변환하기 위해서 사용
+		OriginalFileName = new String(OriginalFileName.getBytes("UTF-8"),"ISO-8859-1");
+		respone.setHeader("Content-Disposition", "attachment; filename=\""+OriginalFileName+"\"");
+		
+		
+		//3.Content-length 헤더 구성(다운로드할 파일의 크기 지정)
+		int filesize = (int) new File(saveFilePath).length();
+		respone.setContentLength(filesize);
+		
+		//[응답HTTP의 바디(본문) 구성] - 입출력 스트림 사용(문자타입기반)
+		OutputStream os = respone.getOutputStream();
+		FileCopyUtils.copy(is, os);// is에서 읽어서 os로 보냄
+		
+		os.flush();
+		os.close();
+		is.close();
+
+	}
+	
 	//강사 공지사항 수정폼 요청하기
 	@GetMapping("/noticeUpdateForm")
-	public String noticeUpdateForm(int class_notice_no,Model model) {
+	public String noticeUpdateForm(int class_notice_no,Model model,HttpSession session) {
+		String mid = (String) session.getAttribute("sessionMid");
+		
 		ClassNoticeDto notice = classNoticeService.getUpdateForm(class_notice_no);
 		model.addAttribute("notice",notice);
 		
+		//공지사항에 보여질 파일
+		List<ClassNoticeDto> newUpload = classNoticeService.getFiles(notice.getClass_hw_no());
+		model.addAttribute("newUpload",newUpload);
+		
+		//해당 강사가 강의하는 강의 목록 가져와야함
+		List<ClassNoticeDto> classNames = classNoticeService.selectByTutorId(mid);
+		model.addAttribute("classNames",classNames);
 		
 		return "mypage/noticeUpdateForm";
 	}
 	
 	//강사 공지사항 수정하기
 	@PostMapping("/noticeUpdate")
-	public String noticeUpdate(ClassNoticeDto classNoticeDto,Model model){
+	public String noticeUpdate(ClassNoticeDto classNotice,Model model) throws Exception{
 		
-		//게시물 수정
-		classNoticeService.noticeUpdate(classNoticeDto);
+		//게시물 수정 - 공지사항 게시판
+		classNoticeService.noticeUpdate(classNotice);
 		
-		//수정한 게시물을 다시 가져오기
-		ClassNoticeDto notice = classNoticeService.getNoticeDetail(classNoticeDto.getClass_notice_no());
-		model.addAttribute("notice",notice);
 		
+		if(classNotice.getClass_hwFile().getOriginalFilename().equals("")) {
+
+			classNotice.setClass_hw_file("");
+			classNotice.setClass_hw_type("");
+			
+		}else {
+			//파일 내용 넣기(현재날짜로 저장하기)
+			String fileName = classNotice.getClass_hwFile().getOriginalFilename();
+			String saveFile = new Date().getTime()+"_"+fileName;
+			
+			//파일 확장자 넣기
+			String ext = fileName.substring(fileName.lastIndexOf(".")+1);
+			classNotice.getClass_hwFile().transferTo(new File("D:/MyWokspace/files/"+saveFile));
+			classNotice.setClass_hw_file(saveFile);
+			
+			//파일 확장자 넣기
+			classNotice.setClass_hw_type(ext);
+		}
+		
+		
+		//게시물 수정 - 공지사항 첨부파일
+		int result = classNoticeService.updateFile(classNotice);
+		
+		if(result == 0) {
+			logger.info("파일 수정 실패~~");
+		}else {
+			//수정한 게시물을 다시 가져오기
+			ClassNoticeDto notice = classNoticeService.getNoticeDetail(classNotice.getClass_notice_no());
+			model.addAttribute("notice",notice);
+			//공지사항에 보여질 파일
+			List<ClassNoticeDto> newUpload = classNoticeService.getFiles(notice.getClass_hw_no());
+			model.addAttribute("newUpload",newUpload);
+		}
+		
+		
+
 		return "mypage/noticedetail";
 	}
 	
 	//강사의 강의공지사항 삭제하기
 	@GetMapping("/noticeDelete")
-	public String noticeDelete(int class_notice_no, HttpServletResponse response) throws Exception {
+	public String noticeDelete(int class_notice_no, HttpSession session) throws Exception {
+		String mid = (String) session.getAttribute("sessionMid");
+		
 		//게시물 삭제 응답/요청
 		classNoticeService.noticeDelete(class_notice_no);
 				
-		return "mypage/mypage_tutor";
+		return "redirect:/mypage/mypage_tutor?mid="+mid;
 	}
 	
 	//강사의 강의공지사항 글쓰기 폼 요청
@@ -186,6 +270,7 @@ public class MyPageController {
 	public String noticeWriteForm( Model model, HttpSession session) {
 		
 		String mid = (String)session.getAttribute("sessionMid");
+		
 		//해당 강사가 강의하는 강의 목록 가져와야함
 		List<ClassNoticeDto> classNames = classNoticeService.selectByTutorId(mid);
 		model.addAttribute("classNames",classNames);
@@ -195,12 +280,34 @@ public class MyPageController {
 	
 	//강사의 강의공지사항 글쓰기 폼 요청
 	@PostMapping("/noticeWrite")
-	public void noticeWrite(ClassNoticeDto class_notice_no, HttpServletResponse response) throws Exception {
+	public String noticeWrite(ClassNoticeDto classNotice,HttpSession session) throws Exception {
+		String mid = (String) session.getAttribute("sessionMid");
+		
 		//게시물쓰기
-		classNoticeService.noticeWrite(class_notice_no);
+		classNoticeService.noticeWrite(classNotice);
 		
+		if(!classNotice.getClass_hwFile().isEmpty()) { //파일을 올렸을 경우에만
+			
+			//파일 내용 넣기(현재날짜로 저장하기)
+			String fileName = classNotice.getClass_hwFile().getOriginalFilename();
+			String saveFile = new Date().getTime()+"_"+fileName;
+			
+			//파일 확장자 넣기
+			String ext = fileName.substring(fileName.lastIndexOf(".")+1);
+			classNotice.getClass_hwFile().transferTo(new File("D:/MyWokspace/files/"+saveFile));
+			classNotice.setClass_hw_file(saveFile);
+	
+			classNotice.setClass_hw_type(ext);
+			
+		} else {
+			classNotice.setClass_hw_file("");
+			classNotice.setClass_hw_type("");
+		}
 		
-				
+		//첨부파일 intsert
+		classNoticeService.noticeFileInput(classNotice);
+		
+		return "redirect:/mypage/mypage_tutor?mid="+mid;
 	}
 	
 	//=============강사의 강의문의 목록===================================
